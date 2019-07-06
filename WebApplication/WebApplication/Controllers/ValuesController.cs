d11d5c11b8980c8a67303137ccf6d0f954585e9d
duc.vo <ducvn256@gmail.com>
2019-07-06 15:10:29 +0700

    commit

--- WebApplication/WebApplication/Controllers/ValuesController.cs
+++ WebApplication/WebApplication/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Doan Minh Tu", "Vo Nhat Duc", "Phuc XO", "Phong Nhu" };
         }
 
         // GET api/values/5
